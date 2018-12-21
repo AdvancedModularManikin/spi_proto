a969e00fa37b63ed6b53721ac297736af5cbc309
Peter O'Hanley <gus@entropicengineering.com>
2018-12-20 19:37:17 -0600

    Documented the spi_callback variable. Part of #6

--- test/blip-test.cpp
+++ test/blip-test.cpp
@@ -7,21 +7,35 @@ using namespace spi_proto;
 
 void
 blip_task(void);
-void (*spi_callback)(struct spi_packet *p) = NULL;
+/* We need to declare a function pointer, the library expects to be linked with
+ * one. It's not necessary that it be an actual function.
+ *
+ * This would also be fine:
+ * void (*spi_callback)(struct spi_packet *p) = NULL;
+ */
+void
+dummy_callback(struct spi_packet *p)
+{
+  /* If we linked with this function and the k66f code simply echoed our
+   * messages, this would receive arguments where p->msg[0] was alternately 60
+   * and 120.
+   */
+}
+void (*spi_callback)(struct spi_packet *p) = dummy_callback;
 
 int main(int argc, char *argv[]) {
 	std::thread remote_thread(datagram_task);
 	std::thread click_thread(blip_task);
-	
+
 	while (1) {
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
-	
+
 	return 0;
-	
+
 }
 
-//functions implemented for port
+//helper function, not related to remote api
 void
 delay_ms(unsigned int ms)
 {
